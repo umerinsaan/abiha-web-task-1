81f56acedaed2233ca1f56f6b86bc9721780cc1c
= <=>
2023-09-28 00:46:57 +0500

    almost completed for desktop

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  cardsImagesPaths: string[] = [];
+  
+  cardsTitles: string[] = [
+    "deep earth".toUpperCase(),
+    "night arcade".toUpperCase(),
+    "soccer team vr".toUpperCase(),
+    "the grid".toUpperCase(),
+    "from up above vr".toUpperCase(),
+    "pocket borealis".toUpperCase(),
+    "the curiosity".toUpperCase(),
+    "make it fisheye".toUpperCase()
+  ];
 
+  constructor() {
+    for (let i = 0; i < 8; i++) {
+      this.cardsImagesPaths.push(`../../../assets/images/desktop/desktop-img-${i + 1}.jpg`);
+    }
+  }
+
+  
 }
